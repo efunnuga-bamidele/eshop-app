863bb7c408f087180051f530f51046ca92327345
Efunnuga Bamidele <bamideleefunnuga9@gmail.com>
2022-10-21 12:30:26 +0100

    ✨ Create Dynamic Products using Props

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,22 +2,32 @@
 //Sytles
 import './Product.css'
 
-export default function Product() {
+export default function Product({ id, title, price, rating, image}) {
+    const handleAddToCart = (e) => {
+            
+    }
     return(
+
 
             <div className="product">
                 <div className="product_info">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, laudantium.</p>
+                    <p>{title}</p>
                     <p className='product_price'>
                         <small>$</small>
-                        <strong>30</strong>
+                        <strong>{price}</strong>
                     </p>
-                    <div className="product_rating">⭐⭐⭐</div>
+                    <div className="product_rating">
+                        {Array(rating)
+                            .fill()
+                            .map((_, i) => (
+                                <p>⭐</p>
+                                ))}
+                    </div>
                 </div>
 
-                <img src="https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpg" alt="product-image" />
+                <img src={image} alt="product-image" />
 
-                <button>Add to Basket</button>
+                <button onClick={(id) => handleAddToCart(id)}>Add to Basket</button>
             </div>
 
     )
